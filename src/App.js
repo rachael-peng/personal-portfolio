2dadecc7863f84b3f952f80e6a101236828da31d
rachael-peng <rachael-peng@users.noreply.github.com>
2024-11-11 21:43:26 -0700

    back

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function App() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Kulim+Park:ital,wght@0,200;0,300;0,400;0,600;0,700;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet"/>
       </header>
-      <p>let me know</p>
       <AppLayout>
         <Routes>
           <Route path="/" element={<HomePage />} />
